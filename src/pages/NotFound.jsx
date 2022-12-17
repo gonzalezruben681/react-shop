aa4cf5c404e6fbe29c7785c01264cd9aab073bb6
Ruben Dario Gonzalez <gonzalezruben681@gmail.com>
2022-12-16 20:08:38 -0600

    add: rutas y algunos componentes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    // TODO: hacer los estilos y la página 
+    return (
+        <div>
+            <p>No encontrado, error 404.</p>
+        </div>
+    );
+}
+
+export default NotFound;
