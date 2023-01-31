d74d6a544427a13ca3cfa2fa7275653fcc3b84dd
Ruben Dario Gonzalez <gonzalezruben681@gmail.com>
2023-01-30 18:06:15 -0600

    add: Eliminar productos del carrito

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -4,17 +4,25 @@ const initialState = {
     cart:[],
 }
 const useInitialState = () => {
-    const [state, setstate] = useState(initialState);
+    const [state, setState] = useState(initialState);
     const addToCart = (payload) => {
-        setstate({
+        setState({
             ...state,
             cart:[...state.cart, payload]
         });
     };
 
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter(items => items.id !== payload.id),
+        });
+    }
+
     return {
         state,
-        addToCart
+        addToCart,
+        removeFromCart
     }
 }
 export default useInitialState;
